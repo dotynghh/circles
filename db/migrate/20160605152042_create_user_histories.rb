bc958d53f87e1d27a25425032032b18b98054142
dotyng_hh <Dotyng_hh@163.com>
2016-06-20 15:16:51 +0800

    delte history

--- db/migrate/20160605152042_create_user_histories.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserHistories < ActiveRecord::Migration
-  def change
-    create_table :user_histories do |t|
-    	t.belongs_to :user
-    	t.integer :blog_id
-      t.timestamps null: false
-    end
-  end
-end
